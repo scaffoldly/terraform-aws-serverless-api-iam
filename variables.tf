05529dbfc788c75ed01eda43d3c77cbaa639eb9b
Christian Nuss <christian.nuss@gmail.com>
2021-07-12 10:29:30 -0700

    Formatting

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "repository_name" {
-  type = string
+  type        = string
   description = "The repository name for the Serverless API (role names are prefixed with this for service-level isololation of privileges)"
 }
