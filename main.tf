1326fb30742b56ffbb83119e4cb7ec12112e3168
Christian Nuss <christian.nuss@gmail.com>
2021-08-30 12:42:33 -0700

    s3:*Encryption*

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "cloudformation" {
       "lambda:*EventSourceMapping*",
       "s3:ListAllMyBuckets",
       "s3:CreateBucket",
-      "s3:SetBucketEncryption",
+      "s3:*Encryption*",
       "s3:*Notification*",
       "sns:Get*",
       "sns:List*",
@@ -256,7 +256,7 @@ data "aws_iam_policy_document" "deployer" {
     actions = [
       "s3:ListAllMyBuckets",
       "s3:CreateBucket",
-      "s3:SetBucketEncryption",
+      "s3:*Encryption*",
       "s3:*Notification*"
     ]
 
